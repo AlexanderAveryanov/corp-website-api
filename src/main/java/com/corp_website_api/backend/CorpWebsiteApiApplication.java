0f78544c523ae161f668235d20ada9db45fd7b29
AlexanderAveryanov <averyanov0595@mail.ru>
2025-09-11 20:42:52 +0300

    Initial commit

---
+++ src/main/java/com/corp_website_api/backend/CorpWebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.corp_website_api.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpWebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpWebsiteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corp_website_api/backend/CorpWebsiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corp_website_api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpWebsiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
