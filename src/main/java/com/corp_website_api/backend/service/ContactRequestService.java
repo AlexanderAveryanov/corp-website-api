061008ea7f7dd72cab3aafac6dbb4dd3a47e6959
AlexanderAveryanov <averyanov0595@mail.ru>
2025-09-16 20:54:56 +0300

    feat(service): Реализовать сервис для работы с заявками

---
+++ src/main/java/com/corp_website_api/backend/service/ContactRequestService.java
@@ -0,0 +1,36 @@
+package com.corp_website_api.backend.service;
+
+import com.corp_website_api.backend.entity.ContactRequest;
+import com.corp_website_api.backend.repository.ContactRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// Cервис слой для бизнес-логики
+// Эта аннотация делает класс Spring-сервисом
+// Регистрирует класс в Spring-контейнере, позволяет использовать @Autowired в других классах
+@Service
+public class ContactRequestService {
+
+    @Autowired // Автоматически внедряет репозиторий
+    private ContactRequestRepository repository;
+
+    public ContactRequest createRequest(ContactRequest request) {
+        return repository.save(request);
+    }
+
+    public List<ContactRequest> getAllRequests() {
+        return repository.findAll();
+    }
+
+    public ContactRequest getRequestById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Заявка не найдена"));
+    }
+
+//    public ContactRequest updateRequestStatus(Long id, String status) {
+//        ContactRequest request = getRequestById(id);
+//        request.setStatus(ContactRequest.RequestStatus.valueOf(status.toUpperCase()));
+//    }
+}
